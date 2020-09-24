0646dcde2f05d7354e1e4d2b6633440b442b8c00
Luvumbu <luvumbu.n@gmail.com>
2020-09-24 19:08:31 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php
+	include("link.html");
+	?>
+
+	<title>Document</title>
+</head>
+
+<body>
+	<header>
+		<?php
+		include("model/header.php");
+		?>
+	</header>
+	<section>
+		<?php
+		include("model/section.php");
+		?>
+	</section>
+
+	<footer>
+		<?php
+		include("model/footer.php");
+		?>
+	</footer>
+</body>
+
+</html>
