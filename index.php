409aa282a333a6663519e5b52015938658a0c5f4
Luvumbu <luvumbu.n@gmail.com>
2020-09-24 20:33:27 +0200

    add mis ok

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
 session_start();
-$REMOTE_ADDR = $_SERVER['REMOTE_ADDR'];
-if ($REMOTE_ADDR == "::1" || $REMOTE_ADDR == "127.0.0.1") {
-	include("class/ip_in.php");
-} else {
-	include("class/ip_out.php");
-}
+include("ip.php");
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,9 +11,7 @@ if ($REMOTE_ADDR == "::1" || $REMOTE_ADDR == "127.0.0.1") {
 	include("link.html");
 	?>
 	<title>Bokonzi</title>
-	<link rel="icon" href="https://pbs.twimg.com/profile_images/1244325575659061249/YjvhVutG_400x400.jpg" type="image/gif" sizes="16x16">
 </head>
-
 <body>
 	<div id="body">
 		<header>
@@ -39,7 +31,7 @@ if ($REMOTE_ADDR == "::1" || $REMOTE_ADDR == "127.0.0.1") {
 			?>
 		</footer>
 		<?php
-		include("vue.html");
+			include("vue.html");
 		?>
 	</div>
 </body>
